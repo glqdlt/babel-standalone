b584acc773cbbd0eaf9c65fe3617317c2921ff61
jhun <jhun@nm-4ones.com>
2018-11-14 09:14:55 +0900

    불변 객체로 변경

--- es6-app.js
+++ es6-app.js
@@ -1,7 +1,7 @@
 const printFunction = () => {
-    let text = document.createElement('P');
+    const text = document.createElement('P');
     text.innerText = 'hi';
-    let zone = document.getElementById('compile-zone');
+    const zone = document.getElementById('compile-zone');
     zone.appendChild(text);
                                 };
 printFunction();
